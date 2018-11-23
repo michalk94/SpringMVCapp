a405c2f42da6613dfecc533f35e7dd9c426a5986
michalk94 <michal.kwit94@gmail.com>
2018-11-23 22:20:12 +0100

    Adding new functionality(searching users by lastname) and refactroing code

--- src/main/java/springmvcproject/springmvcprojectrest/user/UserRepository.java
+++ src/main/java/springmvcproject/springmvcprojectrest/user/UserRepository.java
@@ -2,5 +2,8 @@ package springmvcproject.springmvcprojectrest.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByLastNameContainingIgnoreCase(String lastName);
 }
--- src/main/java/springmvcproject/springmvcprojectrest/user/UserResources.java
+++ src/main/java/springmvcproject/springmvcprojectrest/user/UserResources.java
@@ -3,6 +3,7 @@ package springmvcproject.springmvcprojectrest.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,12 +14,15 @@ public class UserResources {
 
     private UserService userService;
 
-    UserResources(UserService userService){
+    UserResources(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("")
-    List<UserDto>findAll(){
-        return userService.findAll();
+    List<UserDto> findAll(@RequestParam(required = false) String lastName) {
+        if (lastName != null)
+            return userService.findByLastName(lastName);
+        else
+            return userService.findAll();
     }
 }
