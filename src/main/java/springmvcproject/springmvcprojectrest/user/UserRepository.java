0c210d40c8709d08b26458f187b813efa9ee762f
michalk94 <michal.kwit94@gmail.com>
2018-11-23 21:44:01 +0100

    Added new dependency to pom.xml(jpa). Creating User entity

--- src/main/java/springmvcproject/springmvcprojectrest/user/UserRepository.java
+++ src/main/java/springmvcproject/springmvcprojectrest/user/UserRepository.java
@@ -1,4 +1,6 @@
 package springmvcproject.springmvcprojectrest.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
