532a4d6a252b023aa09ed9928a4d26201251f253
michalk94 <michal.kwit94@gmail.com>
2018-11-25 18:45:48 +0100

    Creating class AssetDto

--- src/main/java/springmvcproject/springmvcprojectrest/asset/AssetDto.java
+++ src/main/java/springmvcproject/springmvcprojectrest/asset/AssetDto.java
@@ -1,4 +1,13 @@
 package springmvcproject.springmvcprojectrest.asset;
 
+import lombok.Data;
+
+@Data
 public class AssetDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String serialNumber;
+    private String category;
 }
