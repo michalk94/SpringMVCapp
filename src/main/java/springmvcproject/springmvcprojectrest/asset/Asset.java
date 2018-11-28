7aa877e123b7946326c5fce9482ae6f640769f0f
michalk94 <michal.kwit94@gmail.com>
2018-11-28 18:12:02 +0100

    Added JPQL query to AssetRepo, first step to add funcionality for searching by name or serial number.

--- src/main/java/springmvcproject/springmvcprojectrest/asset/Asset.java
+++ src/main/java/springmvcproject/springmvcprojectrest/asset/Asset.java
@@ -15,8 +15,10 @@ public class Asset {
     private Long id;
     private String name;
     private String description;
+
     @Column(unique = true)
     private String serialNumber;
+
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
--- src/main/java/springmvcproject/springmvcprojectrest/asset/AssetRepository.java
+++ src/main/java/springmvcproject/springmvcprojectrest/asset/AssetRepository.java
@@ -1,7 +1,14 @@
 package springmvcproject.springmvcprojectrest.asset;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+    @Query("select a from Asset a where lower(a.name) like lower(concat('%', :search, '%')) " +
+    "or lower(a.serialNumber) like lower(concat('%', :search, '%'))")
+    List<Asset> findAllByNameOrSerialNumber(@Param("search") String search);
 
 }
