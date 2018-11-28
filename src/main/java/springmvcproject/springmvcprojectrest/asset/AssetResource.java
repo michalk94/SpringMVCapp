c14d5f088c13dcd4a8e813ce546ba98617262452
michalk94 <michal.kwit94@gmail.com>
2018-11-28 18:14:52 +0100

    Refactoring controller(AssetResource)  method findAll

--- src/main/java/springmvcproject/springmvcprojectrest/asset/AssetResource.java
+++ src/main/java/springmvcproject/springmvcprojectrest/asset/AssetResource.java
@@ -4,6 +4,7 @@ package springmvcproject.springmvcprojectrest.asset;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +17,10 @@ public class AssetResource {
     private AssetService assetService;
 
     @GetMapping("")
-    public List<AssetDto> findAll(){
-        return assetService.findAll();
+    public List<AssetDto> findAll(@RequestParam(required = false) String text) {
+        if (text != null)
+            return assetService.findAllByNameOrSerialNumber(text);
+        else
+            return assetService.findAll();
     }
 }
