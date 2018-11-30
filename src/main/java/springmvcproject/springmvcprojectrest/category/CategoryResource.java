dbce2dfb823e8f2eb1d9fd62e6d0483cafdd24c7
michalk94 <michal.kwit94@gmail.com>
2018-11-30 12:32:58 +0100

    Defining service class and controller.

--- src/main/java/springmvcproject/springmvcprojectrest/category/CategoryResource.java
+++ src/main/java/springmvcproject/springmvcprojectrest/category/CategoryResource.java
@@ -1,4 +1,23 @@
 package springmvcproject.springmvcprojectrest.category;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@AllArgsConstructor
 public class CategoryResource {
+
+    private CategoryService categoryService;
+
+    @GetMapping("/names")
+    public List<String> findAllNames(){
+        return categoryService.findAllNames();
+    }
+
 }
--- src/main/java/springmvcproject/springmvcprojectrest/category/CategoryService.java
+++ src/main/java/springmvcproject/springmvcprojectrest/category/CategoryService.java
@@ -1,4 +1,24 @@
 package springmvcproject.springmvcprojectrest.category;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
 public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+
+    List<String> findAllNames(){
+        return categoryRepository.findAll()
+                .stream()
+                .map(Category::getName)
+                .collect(Collectors.toList());
+    }
+
 }
