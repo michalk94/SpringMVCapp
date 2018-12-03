8044abeb4de286074159f16319ab2d1544ecf2bf
michalk94 <michal.kwit94@gmail.com>
2018-12-03 17:03:51 +0100

    Modified exception class

--- src/main/java/springmvcproject/springmvcprojectrest/exception/DuplicateSerialNumberException.java
+++ src/main/java/springmvcproject/springmvcprojectrest/exception/DuplicateSerialNumberException.java
@@ -1,4 +1,8 @@
 package springmvcproject.springmvcprojectrest.exception;
 
-public class DuplicateSerialNumberException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Asset with this serial number already exists")
+public class DuplicateSerialNumberException extends RuntimeException{
 }
