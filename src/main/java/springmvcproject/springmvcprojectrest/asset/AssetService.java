e080d4196f06c3ee88db4b76c2d005df3a7b73ec
michalk94 <michal.kwit94@gmail.com>
2018-12-04 16:02:38 +0100

    Refactoring AssetService, added new functionality to edit assets.

--- src/main/java/springmvcproject/springmvcprojectrest/asset/AssetService.java
+++ src/main/java/springmvcproject/springmvcprojectrest/asset/AssetService.java
@@ -25,18 +25,37 @@ public class AssetService {
                 .collect(Collectors.toList());
     }
 
-    List<AssetDto> findAllByNameOrSerialNumber(String text){
+    List<AssetDto> findAllByNameOrSerialNumber(String text) {
         return assetRepository.findAllByNameOrSerialNumber(text)
                 .stream()
                 .map(assetMapper::toDto)
                 .collect(Collectors.toList());
     }
 
-    AssetDto save(AssetDto asset){
+    AssetDto save(AssetDto asset) {
         Optional<Asset> assetBySerialNo = assetRepository.findBySerialNumber(asset.getSerialNumber());
         assetBySerialNo.ifPresent(asset1 -> {
             throw new DuplicateSerialNumberException();
         });
+        return mapAndSave(asset);
+    }
+
+    Optional<AssetDto> findById(Long id) {
+        return assetRepository.findById(id).map(assetMapper::toDto);
+
+
+    }
+
+    AssetDto update(AssetDto asset){
+        Optional<Asset> assetBySerialNo = assetRepository.findBySerialNumber(asset.getSerialNumber());
+        assetBySerialNo.ifPresent(a -> {
+            if (!a.getId().equals(asset.getId()))
+                throw new DuplicateSerialNumberException();
+        });
+        return mapAndSave(asset);
+    }
+
+    private AssetDto mapAndSave(AssetDto asset){
         Asset assetEntity = assetMapper.toEntity(asset);
         Asset savedAsset = assetRepository.save(assetEntity);
         return assetMapper.toDto(savedAsset);
