5af88fc407bb31a158ed8a8b99ada9f36c7d84e9
michalk94 <michal.kwit94@gmail.com>
2018-12-05 14:50:15 +0100

    Creating new Dto class

--- src/main/java/springmvcproject/springmvcprojectrest/user/UserAssignmentDto.java
+++ src/main/java/springmvcproject/springmvcprojectrest/user/UserAssignmentDto.java
@@ -1,4 +1,16 @@
 package springmvcproject.springmvcprojectrest.user;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class UserAssignmentDto {
+
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long assetId;
+    private String assetName;
+    private String assetSerialNumber;
 }
