d5b65c062281f6c4c189309a4a44311e077c2a84
michalk94 <michal.kwit94@gmail.com>
2018-12-06 22:44:26 +0100

    Defined mapper class

--- src/main/java/springmvcproject/springmvcprojectrest/user/UserAssignmentMapper.java
+++ src/main/java/springmvcproject/springmvcprojectrest/user/UserAssignmentMapper.java
@@ -1,4 +1,20 @@
 package springmvcproject.springmvcprojectrest.user;
 
+import springmvcproject.springmvcprojectrest.asset.Asset;
+import springmvcproject.springmvcprojectrest.assignment.Assignment;
+
 public class UserAssignmentMapper {
+
+    static UserAssignmentDto toDto(Assignment assignment){
+        UserAssignmentDto dto = new UserAssignmentDto();
+        dto.setId(assignment.getId());
+        dto.setAssetId(assignment.getId());
+        dto.setStart(assignment.getStart());
+        dto.setEnd(assignment.getEnd());
+        Asset asset = assignment.getAsset();
+        dto.setAssetId(asset.getId());
+        dto.setAssetName(asset.getName());
+        dto.setAssetSerialNumber(asset.getSerialNumber());
+        return dto;
+    }
 }
