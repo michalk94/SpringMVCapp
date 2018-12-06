2c484cadcfd7b6bad3f0d0a0e122f485ef4afeea
michalk94 <michal.kwit94@gmail.com>
2018-12-06 22:44:57 +0100

    Defined exception class

--- src/main/java/springmvcproject/springmvcprojectrest/user/UserNotFoundException.java
+++ src/main/java/springmvcproject/springmvcprojectrest/user/UserNotFoundException.java
@@ -1,4 +1,9 @@
 package springmvcproject.springmvcprojectrest.user;
 
-public class UserNotFoundException {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No user with this id")
+public class UserNotFoundException extends RuntimeException {
 }
