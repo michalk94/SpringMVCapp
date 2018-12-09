5e2226504c794069171858177c9f8396b03594a9
michalk94 <michal.kwit94@gmail.com>
2018-12-09 19:20:32 +0100

    Defining mapper and dto classes

--- src/main/java/springmvcproject/springmvcprojectrest/asset/AssetAssignmentDto.java
+++ src/main/java/springmvcproject/springmvcprojectrest/asset/AssetAssignmentDto.java
@@ -1,4 +1,17 @@
 package springmvcproject.springmvcprojectrest.asset;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class AssetAssignmentDto {
+
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String pesel;
 }
--- src/main/java/springmvcproject/springmvcprojectrest/asset/AssetAssignmentMapper.java
+++ src/main/java/springmvcproject/springmvcprojectrest/asset/AssetAssignmentMapper.java
@@ -1,4 +1,21 @@
 package springmvcproject.springmvcprojectrest.asset;
 
+import springmvcproject.springmvcprojectrest.assignment.Assignment;
+import springmvcproject.springmvcprojectrest.user.User;
+
 public class AssetAssignmentMapper {
+
+    static AssetAssignmentDto toDto(Assignment assignment){
+        AssetAssignmentDto dto = new AssetAssignmentDto();
+        dto.setId(assignment.getId());
+        dto.setStart(assignment.getStart());
+        dto.setEnd(assignment.getEnd());
+
+        User user = assignment.getUser();
+        dto.setUserId(user.getId());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+        dto.setPesel(user.getPesel());
+        return dto;
+    }
 }
